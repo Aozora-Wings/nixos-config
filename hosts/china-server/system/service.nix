b80a5d461a7d06179cf4fce78c43ea07e74566ba
wt <wt@qkzy.net>
2026-01-17 11:15:57 +0800

    test

--- hosts/china-server/system/service.nix
+++ hosts/china-server/system/service.nix
@@ -3,12 +3,14 @@
 , install-config
 , unstable
 , secrets_file
+, config
 , ...
 }:
 let
 
   username = install-config.username;
   hyprlandConfigPath = "${toString ../config/hyprland.conf}";
+  keyExists = builtins.pathExists "/home/${username}/.ssh/vw_wt";
 
 in
 {
@@ -45,17 +47,25 @@ in
     #     videoDrivers = ["nvidia"];
     # }
   };
-  age = {
-      identityPaths = [
-    /home/${install-config.username}/.ssh/vw_wt
-  ];
-    secrets."azure-token" = {
+  config = lib.mkIf keyExists {
+    # 这里是完整的 age/secrets 配置
+    age.identityPaths = [
+      "/home/${username}/.ssh/vw_wt"
+    ];
+    
+    age.secrets."azure-token" = {
       file = secrets_file.azure;
-      owner = install-config.username;
+      owner = username;
     };
-    secrets."web" = {
+    
+    age.secrets."web" = {
       file = secrets_file.web;
-      owner = install-config.username;
+      owner = username;
+    };
+    
+    # 其他依赖 age 的配置也可以放在这里
+    some-other-service.config = {
+      tokenFile = config.age.secrets."azure-token".path;
     };
   };
 
